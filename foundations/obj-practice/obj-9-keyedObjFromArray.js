f18b21c29585087ff1f01abf75dea8fe130e3806
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2025-08-11 15:46:22 +1200

    Finished the obj problems for this part

---
+++ foundations/obj-practice/obj-9-keyedObjFromArray.js
@@ -0,0 +1,33 @@
+function groupById(obj){
+    /*let usersById = {}
+    for(item in obj){
+        usersById[item.id] = item;
+    }
+    return usersById;*/
+    let usersById = {};
+    for (item in obj){
+
+        let id = obj[item].id;
+        usersById[id] = obj[item];
+    }
+    return usersById
+
+}
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
+console.log(usersById)
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
