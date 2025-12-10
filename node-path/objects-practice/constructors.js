ebd177a543858808b253d15cb8f967b82e7cf3ea
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2025-12-10 15:17:47 +1300

    Did a bit more

--- node-path/objects-practice/constructors.js
+++ node-path/objects-practice/constructors.js
@@ -20,6 +20,36 @@ function Book(title, author, pages, read){
     }
 }
 
+function Hero(name, level){
+    this.name=name;
+    this.level=level;
+}
+
+function Warrior(name, level, weapon){
+    Hero.call(this, name, level);
+    this.weapon=weapon;
+}
+function Healer(name, level, spell){
+    Hero.call(this, name, level);
+    this.spell=spell;
+}
+
+let hero1 = new Warrior('Bjorn', 1, 'axe');
+let hero2 = new Healer('Kanin', 1, 'cure');
 
 const newBook = new Book("Alpha", "Beta", 256, false);
 console.log(newBook.info());
+console.log(hero1)
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+Hero.prototype.greet=function(){
+    return `${this.name} says hello.`;
+}
+
+Warrior.prototype.attack=function(){
+    return `${this.name} attacks with the ${this.weapon}.`;
+}
+
+Healer.prototype.heal=function(){
+    return `${this.name} casts ${this.spell}.`;
+}
