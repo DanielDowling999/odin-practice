3faf813d23717a0b94aeace872497f93b3e17111
DanielDowling999 <62310275+DanielDowling999@users.noreply.github.com>
2026-02-18 11:24:55 +1300

    Not many examples to go through for scope, was mostly reading

--- node-path/objects-practice/scope.js
+++ node-path/objects-practice/scope.js
@@ -1,1 +1,13 @@
-console.log("It Works!")
+const age =100;
+
+function go() {
+    const hair = "Blonde";
+    console.log(hair);
+}
+
+if (1 == 1){
+    var cool = true;
+}
+
+console.log(age);
+console.log(cool);
